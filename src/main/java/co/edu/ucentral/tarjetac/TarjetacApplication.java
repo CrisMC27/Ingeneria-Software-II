b87222147aea9e5048ae8594315a3eb662fa2137
Cris <cristianmartinez.10@hotmail.com>
2024-04-04 21:26:09 -0500

    Commit base master

---
+++ src/main/java/co/edu/ucentral/tarjetac/TarjetacApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.tarjetac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarjetacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarjetacApplication.class, args);
+	}
+
+}
