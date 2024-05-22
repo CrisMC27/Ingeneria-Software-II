463bd143474b1368f4c328146e9f49d21ab00093
Cris <cristianmartinez.10@hotmail.com>
2024-05-22 08:45:51 -0500

    Commit: Update HU11

--- src/main/java/co/edu/ucentral/tarjetac/dto/PagosDto.java
+++ src/main/java/co/edu/ucentral/tarjetac/dto/PagosDto.java
@@ -1,7 +1,6 @@
 package co.edu.ucentral.tarjetac.dto;
 
 
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.io.Serializable;
@@ -14,10 +13,6 @@ import java.io.Serializable;
 
 public class PagosDto implements Serializable {
     private long serial;
-
-   //@NotNull(message = "El número de la tarjeta es obligatorio")
     private long numerotar;
-
-    //@NotNull(message = "El monto a pagar es obligatorio")
     private long monto;
 }
