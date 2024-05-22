fe03c19183113132fa121edfaf39347f21cc1e99
Cris <cristianmartinez.10@hotmail.com>
2024-05-22 08:41:57 -0500

    Commit: Update en Tarjetas

--- src/main/java/co/edu/ucentral/tarjetac/dto/TarjetasDto.java
+++ src/main/java/co/edu/ucentral/tarjetac/dto/TarjetasDto.java
@@ -14,19 +14,15 @@ import java.util.Date;
 
 public class TarjetasDto implements Serializable {
     private long serial;
-    @NotBlank(message = "El codigo de la tarjeta es obligatorio")
     private long numerotarjeta;
     @NotBlank(message = "La fecha de vencimiento es obligatoria")
     private Date fecha_vec;
-    @NotBlank(message = "El documento es obligatorio")
     private int documento;
-    @NotBlank(message = "El cupo es obligatorio")
     private long cupo;
     @NotBlank(message = "La fecha de corte es obligatoria")
     private Date fecha_cor;
     @NotBlank(message = "La fecha limite de pago es obligatoria")
     private Date fecha_lim;
-    @NotBlank(message = "El saldo es obligatorio")
     private long saldo;
 
 }
